728206981b6dc9dec4025eacb4eb2c564b11285d
CThurler <c.thurlerr@gmail.com>
2025-05-29 10:03:14 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100 text-3xl font-bold text-blue-800">
+      Tailwind funcionando com React! 🚀
+    </div>
+  );
+}
+
+export default App;
